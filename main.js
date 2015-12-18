d8ab490c7440e3fc7026dc76648c0339ea856af2
root <root@ip-172-31-60-46.ec2.internal>
2015-12-18 16:00:45 +0000

    some slight refactoring

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var apiKey = "b8df41e2f08c3325"
 var latitude = undefined;
 var longitude = undefined;
 var i =0;
+
 // Get their location.
 var options = {
   //enableHighAccuracy: true,
@@ -9,8 +10,14 @@ var options = {
   maximumAge: 0
 };
 
+// When the page is rendered, grab location.
+// Make sure you load jQuery in your html doc FIRST!!
+jQuery(document).ready(function($) {
+ navigator.geolocation.getCurrentPosition(success, error, options);
+  console.log("http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/"+latitude + "," +longitude + ".json");
+});
 
-
+//On successful browser location, get weather and update div.
 function success(pos) {
   crd = pos.coords;
   longitude = crd.longitude;
@@ -25,47 +32,22 @@ function success(pos) {
        console.log(parsed_json)
        var location = parsed_json['location']['city'];
        var temp_f = parsed_json['current_observation']['temp_f'];
-       document.getElementById('weather').innerHTML = ("Current temperature in " + longitude+ " , "  + latitude + " is: " + temp_f);
+       document.getElementById('weather').innerHTML = ("Current temperature in " + location + " is: " + temp_f);
      }
    });
   console.log("geolocations successful", latitude, longitude);
 };
 
+// If something goes wrong getting weather.
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
 
 
- 
+// In case I need to take a nap.
 function sleepFor( sleepDuration ){
     var now = new Date().getTime();
     while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
 }
 
 
-//console.log(latitude, longitude);
-
-
-
-
-// Make sure you load jQuery in your html doc FIRST!!
-jQuery(document).ready(function($) {
- navigator.geolocation.getCurrentPosition(success, error, options);
- // Retry a few times because getCurrentPosition is REALLY flakey.
- //sleepFor(10000);
-
- /* $.ajax({
-     //url:"http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/France/Paris.json",
-      url: "http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/"+latitude + "," +longitude + ".json",
-    //  url: "http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/47.5311399,-122.029461-122.029461.json",
-    dataType: "jsonp",
-    success: function(parsed_json) {
-      console.log(parsed_json)
-      var location = parsed_json['location']['city'];
-      var temp_f = parsed_json['current_observation']['temp_f'];
-      document.getElementById('weather').innerHTML = ("Current temperature in " + longitude+ " , "  + latitude + " is: " + temp_f);
-    }
-  });*/
-
-  console.log("http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/"+latitude + "," +longitude + ".json");
-});
