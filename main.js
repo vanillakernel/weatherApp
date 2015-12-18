6535e07fc1cf605d0f1ec8e50f95e640ca3114c5
root <root@ip-172-31-60-46.ec2.internal>
2015-12-18 14:17:48 +0000

    ugh

--- main.js
+++ main.js
@@ -19,17 +19,36 @@ function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+ 
+function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+}
+
+
+function sleepThenAct(){ sleepFor(2000); console.log(latitude,longitude); }
+
+//console.log(latitude, longitude);
+sleepThenAct();
+
+
+
 
 // Make sure you load jQuery in your html doc FIRST!!
 jQuery(document).ready(function($) {
+
+
   $.ajax({
-    url: "http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/IA/Cedar_Rapids.json",
+      http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/
+      //url: "http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/"+latitude + "," +longitude + ".json",
+      //url: " http://api.wunderground.com/api/b8df41e2f08c3325/geolookup/conditions/q/47.5311399,-122.029461-122.029461.json",
     dataType: "jsonp",
     success: function(parsed_json) {
       var location = parsed_json['location']['city'];
       var temp_f = parsed_json['current_observation']['temp_f'];
-      document.getElementById('weather').innerHTML = ("Current temperature in " + longitude + location + " is: " + temp_f);
+      document.getElementById('weather').innerHTML = ("Current temperature in " + longitude+ " , "  + latitude + " is: " + temp_f);
     }
   });
 
